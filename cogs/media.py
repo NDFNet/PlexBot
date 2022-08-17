2a1a2ac7c42fc0e324449bf09d6ff5bc040f2309
Frankie B <floppydisk05@aol.com>
2022-08-18 00:10:26 +0100

    no earrape :norton:

--- cogs/media.py
+++ cogs/media.py
@@ -1,3 +1,4 @@
+from operator import contains
 from nextcord.ext import commands
 from PlexBot.userchecks import UserChecks
 from PlexBot.plex import plex
@@ -11,11 +12,15 @@ class Media(commands.Cog):
         
     @commands.command(brief="Plays the requested song")
     async def play(self, ctx: commands.Context, *, arg):
+        if contains(arg, "earrape"):
+            # no earrape :norton:
+            await ctx.reply("https://cdn.discordapp.com/emojis/819773288384430152.png")
+            return
         await UserChecks.do_blacklist_check(ctx)
         await UserChecks.do_vc_check(ctx)
         await UserChecks.do_bot_in_vc_check(ctx)
         await UserChecks.do_same_vc_check(ctx)
-                
+
         tracks = plex.search(arg, mediatype='track', limit=10)
         try: track: Track = tracks[0]
         except IndexError: await ctx.reply(f"No results for \"{arg}\""); return

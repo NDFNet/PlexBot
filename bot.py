be418075bf40f4750c5340b85f4027f443a4c582
Frankie B <floppydisk05@aol.com>
2022-08-26 02:15:40 +0100

    Add blacklist command ’n’ shit
    Signed-off-by: Frankie B <floppydisk05@aol.com>

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 import nextcord
 from nextcord.ext import commands
 
-from PlexBot.config import discord_prefix, discord_token, VERSION
+from PlexBot.config import discord_prefix, discord_token, VERSION, blacklist, discord_owners
 from PlexBot.logging import log
 
 log.info(f"Starting PlexBot {VERSION}")
 
 # Intents and bot
 intents = nextcord.Intents.all()
-bot = commands.Bot(command_prefix=discord_prefix, intents=intents)
+bot = commands.Bot(command_prefix=discord_prefix, intents=intents, owner_ids=discord_owners)
 bot.load_extension("cogs.main")
 bot.load_extension("cogs.media")
 bot.load_extension("cogs.owner")
@@ -17,6 +17,18 @@ bot.load_extension("cogs.owner")
 async def on_ready():
     log.info(f"Logged into Discord as {bot.user}")
     
+@bot.check
+async def blacklist_check(ctx):
+    if ctx.author.id in blacklist:
+        log.error(f"User {ctx.author} is on blacklist")
+        return False
+    else:
+        return True
+
+@bot.event
+async def on_command_error(ctx: commands.Context, error):
+  if isinstance(error, commands.CheckFailure):
+      log.error(f"User check for {ctx.author} failed: {error}")
 
 # Start the bot
 bot.run(discord_token)
--- cogs/owner.py
+++ cogs/owner.py
@@ -1,17 +1,61 @@
-from ast import alias
-import sys
+import json
+import nextcord
 from nextcord.ext import commands
 from PlexBot import config
+from PlexBot.logging import log
 
 class Owner(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, bot: commands.Bot):
+        self.bot: commands.Bot = bot
         
-    @commands.command(brief="Shuts down the bot", alias=["die", "ceasetobe"])
+    async def cog_check(self, ctx):
+        if not await self.bot.is_owner(ctx.author): return False
+        else: return True
+        
+    @commands.command(brief="Shuts down the bot", alias=["die", "ceasetobe"], )
     async def kill(self, ctx: commands.Context):
-        if ctx.author.id in config.discord_owners:
-            await ctx.send("**Shutting down...**")
-            sys.exit(0)
+        #if not bot.is_owner(ctx.author): return
+        log.info("Shutdown triggered by command")
+        await ctx.send("**Shutting down...**")
+        await self.bot.close()
+            
+    @commands.command(brief="Blacklists the specified user", usage="[user]")
+    async def blacklist(self, ctx: commands.Context, user: nextcord.User = None):
+        if user:
+            user_id = 0
+            try:
+                user_id = user.id
+            except commands.UserNotFound:
+                await ctx.send("User not found")
+                return
+            if ctx.author.id == user_id:
+                await ctx.send("You can't blacklist yourself!")
+                return
+            with open("config.json",'r') as jsonfile:
+                json_content = json.load(jsonfile)
+
+            if user_id in json_content["discord"]["blacklist"]:
+                json_content["discord"]["blacklist"].remove(user_id)
+                config.blacklist.remove(user_id)
+                await ctx.reply(f"Unblacklisted {user}")
+            else:
+                json_content["discord"]["blacklist"].append(user_id)
+                config.blacklist.append(user_id)
+                await ctx.reply(f"Blacklisted {user}")
+
+            with open("config.json",'w') as jsonfile:
+                json.dump(json_content, jsonfile, indent=4)
+        else: # If no user specified, show blacklist
+            if len(config.blacklist) == 0:
+                await ctx.send("Blacklist is empty!")
+                return
+            blacklist = "```\n"
+            for user_id in config.blacklist:
+                currentuser = await self.bot.fetch_user(user_id)
+                blacklist += f"{currentuser.name}\n"
+            blacklist += "```"
+            await ctx.send(blacklist)
+            
         
 def setup(bot):
     bot.add_cog(Owner(bot))
